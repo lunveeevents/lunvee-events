64364d8d9342554f7004f3c3fea069aba892b551
lunveeevents <lunveeevents@gmail.com>
2026-01-07 18:52:01 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,1 +1,21 @@
 
+import { EventType } from './types';
+
+export const THEME_COLORS = {
+  primary: 'indigo-600',
+  secondary: 'slate-800',
+  accent: 'amber-500',
+  bg: 'slate-50'
+};
+
+export const EVENT_TYPES: EventType[] = [
+  EventType.FULL_WEDDING,
+  EventType.ONLY_WEDDING,
+  EventType.BIRTHDAY,
+  EventType.PRIVATE_PARTY,
+  EventType.CONCERT,
+  EventType.CORPORATE,
+  EventType.CONFERENCE,
+  EventType.OTHERS
+];
+
