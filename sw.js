59cd0feeac2f65669dcd820f80c094a6cfde9e84
lunveeevents <lunveeevents@gmail.com>
2026-01-07 18:54:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,34 @@
 
+const CACHE_NAME = 'lunvee-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
